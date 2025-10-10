defdc949e62102bf5f4f0b2c100a6340ba68a4d5
Guilherme dos Santos Damascena <guisantosdam2@gmail.com>
2025-10-10 16:13:32 -0300

    Adição de Hover nos btn. Adição de responsividade. Adição de função no btnSair - Login.

--- HelpBox/frmLogin.cs
+++ HelpBox/frmLogin.cs
@@ -19,7 +19,25 @@ namespace HelpBox
 
         private void btnSair_Click(object sender, EventArgs e)
         {
+            // Define a mensagem e o título da caixa de diálogo
+            string mensagem = "Deseja realmente sair?";
+            string titulo = "Confirmação de Saída";
 
+            // Define os botões que a caixa terá (Sim e Não) e um ícone de pergunta
+            MessageBoxButtons botoes = MessageBoxButtons.YesNo;
+            MessageBoxIcon icone = MessageBoxIcon.Question;
+
+            // Mostra a caixa de diálogo e guarda o resultado do clique do usuário
+            DialogResult resultado = MessageBox.Show(mensagem, titulo, botoes, icone);
+
+            // verifica  se o botão clicado pelo usuário foi o "Sim"
+            if (resultado == DialogResult.Yes)
+            {
+            // Se o usuário clicou em "Sim", fecha td
+                Application.Exit();
+            }
+            // Se o usuário clicou em "Não", o código dentro do 'if' é ignorado, 
+            // o método termina, e nada acontece,.
         }
 
         private void btnEntrar_Click(object sender, EventArgs e)
